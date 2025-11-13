0c36e988296922cb731cb00833aabb4ad64535a3
buttercreamlatte <139762072+buttercreamlatte@users.noreply.github.com>
2025-11-13 13:19:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import numpy as np
 
 def load_data():
     url = "https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv"
@@ -14,9 +13,9 @@ df = load_data()
 
 st.sidebar.header("Filter Options")
 country = st.sidebar.selectbox("Select Country", df['Country'].unique())
-metric = st.sidebar.selectbox("Select Metric", ["Confirmed, Recovered, Deaths"])
+metric = st.sidebar.selectbox("Select Metric", ["Confirmed", "Recovered", "Deaths"])
 filtered_df = df[df['Country'] == country]
-fig = px.line(filtered_df, x='Date', y=metric,title=f"{metric} Cases in {Country}")
+fig = px.line(filtered_df, x='Date', y=metric,title=f"{metric} Cases in {country}")
 
 st.plotly_chart(fig, use_container_width=True)
 
